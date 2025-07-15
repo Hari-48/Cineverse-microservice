d804902850cf29947146c72c5a5c4d4e0b8cb1df
Hari-0225 <harihara.sudhan@finsurge.tech>
2025-07-15 14:08:25 +0530

    api-gateway-service

---
+++ gateway-service-api/src/main/java/com/hari/gateway_service_api/filter/JwtValidationGatewayFilterFactory.java
@@ -0,0 +1,40 @@
+package com.hari.gateway_service_api.filter;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.gateway.filter.GatewayFilter;
+import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+public class JwtValidationGatewayFilterFactory extends
+        AbstractGatewayFilterFactory<Object> {
+
+    private final WebClient.Builder webClientBuilder;
+    public JwtValidationGatewayFilterFactory(WebClient.Builder webClientBuilder,
+                                             @Value("${auth.service.url}") String authServiceUrl) {
+        this.webClientBuilder = webClientBuilder.baseUrl(authServiceUrl).build().mutate();
+    }
+
+    @Override
+    public GatewayFilter apply(Object config) {
+        return (exchange, chain) -> {
+            String path = exchange.getRequest().getPath().value();
+            String token =
+                    exchange.getRequest().getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+            if (token == null || !token.startsWith("Bearer ")) {
+                exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+                return exchange.getResponse().setComplete();
+            }
+            return webClientBuilder.build().get()
+                    .uri("lb://AUTH-SERVICE/auth/validate")
+                    .header(HttpHeaders.AUTHORIZATION, token)
+                    .retrieve()
+                    .toBodilessEntity()
+                    .then(chain.filter(exchange));
+        };
+    }
+}
