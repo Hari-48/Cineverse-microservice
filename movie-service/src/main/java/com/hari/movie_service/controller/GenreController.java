bc5725d5aee97a1e216f04b695b401d62f5429fe
Hari-0225 <harihara.sudhan@finsurge.tech>
2025-07-21 17:04:34 +0530

    api config

--- movie-service/src/main/java/com/hari/movie_service/controller/GenreController.java
+++ movie-service/src/main/java/com/hari/movie_service/controller/GenreController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/genre")
+@RequestMapping("movie/genre")
 @RequiredArgsConstructor
 public class GenreController {
 
